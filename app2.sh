37fe067ca518fcc361096badf77f5f3995b7cde2
ankiambom gilbert <jasonmysons@gmail.com>
2022-05-21 07:45:21 +0000

    first version

---
+++ app2.sh
@@ -0,0 +1,8 @@
+#this is just a testing code
+#trying to work with git
+# I love Programming and i will do well in it
+echo " I love Devops"
+echo " Devops is good"
+echo " i will be an exellent Devops Engineer"
+echo " God helping me "
+
